aff12a921d96fe472d7bbc3f3181a758630e3a8d
ei17ringo <ei17ringo@gmail.com>
2016-01-20 14:58:03 +0800

    宿題サンプル

--- bbs.php
+++ bbs.php
@@ -1,44 +1,65 @@
 <?php
-    // セッションを使うことを定義
-    session_start();
 
-    // セッションへデータの保存
-    // $_SESSION["site_title"] = "Online_bbs";
-
-
-    // if (isset($_SESSION["nickname"])) {
-    //     // セッションからデータの取得
-    //     echo $_SESSION["nickname"];
-    // }
-
-    $db = mysqli_connect('mysql105.phy.lolipop.lan','LAA0686173','nexseed1204');
-    mysqli_select_db($db,'LAA0686173-onelinebbs');
-    mysqli_set_charset($db,'utf8');
-
-?>
-<?php
-    // var_dump($_POST);
+  //echo 'POST送信された！';
+  //データベースに接続
+  // ステップ1.db接続
+  $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
     
-    // DBに保存
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      // リクエストメソッドがPOSTだった場合のみ処理
+  // 接続するためのユーザー情報
+  $user = 'root';
+  $password = '';
 
-      // mysql_real_escape_string()について
-      // inputタグに悪意あるユーザーがSQL文などを入力した際に、
-      // ただの文字列として受け取るように文字をエスケープする関数
-      $nickname = mysqli_real_escape_string($db,$_POST['nickname']);
-      $comment = mysqli_real_escape_string($db,$_POST['comment']);
+  // DB接続オブジェクトを作成
+  $dbh = new PDO($dsn,$user,$password);
 
-      $sql = sprintf('INSERT INTO posts SET nickname="%s", comment="%s", created=NOW()',
-            $nickname,
-            $comment
-      );
+  // 接続したDBオブジェクトで文字コードutf8を使うように指定
+  $dbh->query('SET NAMES utf8');
 
-      $_SESSION["nickname"] = $nickname;
+  //POST送信が行われたら、下記の処理を実行
+  //テストコメント
+  if(isset($_POST) && !empty($_POST)){
 
-      mysqli_query($db,$sql);
-      header('Location: bbs.php');
+
+
+    //SQL文作成(INSERT文)
+    $sql = "INSERT INTO `posts`(`nickname`, `comment`, `created`) ";
+    $sql .="VALUES ('".$_POST['nickname']."','".$_POST['comment']."',now())";
+
+    //var_dump($sql);
+    //INSERT文実行
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
+  }
+
+  //SQL文作成(SELECT文)
+  $sql = 'SELECT * FROM `posts`';
+  
+  //SQL文実行
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  $posts = array();
+
+  //var_dump($stmt);
+  while(1){
+
+    //実行結果として得られたデータを表示
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($rec == false){
+      break;
     }
+
+    $posts[]=$rec;
+    // echo $rec['id'];
+    // echo $rec['nickname'];
+    // echo $rec['comment'];
+    // echo $rec['created'];
+
+
+  }
+    //データベースから切断
+    $dbh=null;
 ?>
 
 <!DOCTYPE html>
@@ -47,141 +68,21 @@
   <meta charset="UTF-8">
   <title>セブ掲示版</title>
 
-  <!-- CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" href="assets/css/form.css">
-  <link rel="stylesheet" href="assets/css/timeline.css">
-  <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
+    <form action="bbs.php" method="post">
+      <input type="text" name="nickname" placeholder="nickname" required>
+      <textarea type="text" name="comment" placeholder="comment" required></textarea>
+      <button type="submit" >つぶやく</button>
+    </form>
 
-  <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header page-scroll">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#page-top"><span class="strong-title"><i class="fa fa-linux"></i> Oneline bbs</span></a>
-          </div>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav navbar-right">
-<!--                   <li class="hidden">
-                      <a href="#page-top"></a>
-                  </li>
-                  <li class="page-scroll">
-                      <a href="#portfolio">Portfolio</a>
-                  </li>
-                  <li class="page-scroll">
-                      <a href="#about">About</a>
-                  </li>
-                  <li class="page-scroll">
-                      <a href="#contact">Contact</a>
-                  </li> -->
-              </ul>
-          </div>
-          <!-- /.navbar-collapse -->
-      </div>
-      <!-- /.container-fluid -->
-  </nav>
-
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 content-margin-top">
-        <form action="bbs.php" method="post">
-          <div class="form-group">
-            <div class="input-group">
-              <?php // echo '<input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" value="' . $_SESSION["nickname"] . '" required>'  ?>
-              
-              <?php 
-                  if (isset($_SESSION["nickname"])) {
-                      echo sprintf('<input type="text" name="nickname" class="form-control"
-                       id="validate-text" placeholder="nickname" value="%s" required>',
-                          $_SESSION["nickname"]
-                      );
-                  } else {
-                      echo '<input type="text" name="nickname" class="form-control"
-                       id="validate-text" placeholder="nickname" required>';
-                  }
-              ?>
-
-              <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
-            </div>
-            
-          </div>
-
-          <div class="form-group">
-            <div class="input-group" data-validate="length" data-length="4">
-              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required></textarea>
-              <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
-            </div>
-          </div>
-
-          <button type="submit" class="btn btn-primary col-xs-12" disabled>つぶやく</button>
-        </form>
-      </div>
-
-      <div class="col-md-8 content-margin-top">
-        <?php
-            // データの取得と表示
-            $sql = 'SELECT * FROM posts ORDER BY `created` DESC';
-            $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
-        ?>
-
-        <div class="timeline-centered">
-
-        <?php while ($post = mysqli_fetch_assoc($posts)): ?>
-
-        <article class="timeline-entry">
-
-            <div class="timeline-entry-inner">
-
-                <div class="timeline-icon bg-success">
-                    <i class="entypo-feather"></i>
-                    <i class="fa fa-cogs"></i>
-                </div>
-
-                <div class="timeline-label">
-                    <h2><a href="#"><?php echo $post['nickname'] ?></a> <span><?php echo $post['created'] ?></span></h2>
-                    <p><?php echo $post['comment'] ?></p>
-                </div>
-            </div>
-
-        </article>
-        <?php endwhile; ?>
-
-        <article class="timeline-entry begin">
-
-            <div class="timeline-entry-inner">
-
-                <div class="timeline-icon" style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg);">
-                    <i class="entypo-flight"></i> +
-                </div>
-
-            </div>
-
-        </article>
-
-      </div>
-
-    </div>
-  </div>
-
-
-
-
-
-  
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="assets/js/bootstrap.js"></script>
-  <script src="assets/js/form.js"></script>
+<?php
+foreach ($posts as $post) { ?>
+    <h2><a href="#"><?php echo $post['nickname'];?></a> <span><?php echo $post['created'];?></span></h2>
+    <p><?php echo $post['comment'];?></p>
+<?php
+}
+?>
 </body>
 </html>
 
